918f9bd4feb4f349384d31155ddce68a14f1d07f
Praveen Dias <praveendias1180@gmail.com>
2022-05-13 18:59:48 +0530

    Set add and update

--- sets.py
+++ sets.py
@@ -2,6 +2,15 @@ fruits = {'apple', 'orange', 'banana'}
 
 print(fruits)
 
-fruits = set(['orange', 'apple', 'mango'])
+fruits_2 = set(['orange', 'apple', 'mango', 'grapes'])
 
-print(fruits)
+fruits_2.add('pinapple')
+
+fruits_2.update(['strawberry', 'dragonfruit'])
+
+print(fruits_2)
+
+print(len(fruits_2))
+
+# for fruit in fruits_2:
+#     print(fruit)
