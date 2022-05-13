e92469c8d76197f785d455c9e64ecd5be0a52108
Praveen Dias <praveendias1180@gmail.com>
2022-05-14 00:14:52 +0530

    Python set operations

--- set-ops.py
+++ set-ops.py
@@ -1,5 +1,6 @@
 A = {1, 2, 3, 4, 5}
 B = {1, 3, 5, 7, 9}
+C = {}
 
 print(A | B)
 print(A.union(B))
@@ -9,3 +10,6 @@ print(A.intersection(B))
 
 print(A - B)
 print(A.difference(B))
+
+print(type(A))
+print(type(C))
