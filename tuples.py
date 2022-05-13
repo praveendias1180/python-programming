4a34bfa4d73a1d52a762769537265da2ae210542
Praveen Dias <praveendias1180@gmail.com>
2022-05-14 00:48:57 +0530

    Tuples performance

--- tuples.py
+++ tuples.py
@@ -1,3 +1,5 @@
+import sys
+
 sample = 'computer'
 
 my_list = list(sample)
@@ -12,3 +14,13 @@ print(type(tuple_1))
 
 tuple_2 = 'c', 'a', 'b', 20, 4.1
 print(tuple_2)
+
+my_tuple = ('car', 'pen', 'ice')
+# my_tuple[0] = 'bed'
+a, b, c = my_tuple
+print(a, b, c)
+
+test_list = [1, 2, 3, 4, 5]
+test_tuple = (1, 2, 3, 4, 5)
+print(sys.getsizeof(test_list))
+print(sys.getsizeof(test_tuple))
