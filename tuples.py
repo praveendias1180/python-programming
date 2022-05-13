3215ff401b8f357e52fd9dacc27f99b714c786f4
Praveen Dias <praveendias1180@gmail.com>
2022-05-14 00:17:24 +0530

    Tuples start

---
+++ tuples.py
@@ -0,0 +1,14 @@
+sample = 'computer'
+
+my_list = list(sample)
+print(my_list)
+
+my_set = set(sample)
+print(my_set)
+
+tuple_1 = ('a', 'b', 20, 4.1)
+print(tuple_1)
+print(type(tuple_1))
+
+tuple_2 = 'c', 'a', 'b', 20, 4.1
+print(tuple_2)
