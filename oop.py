3ccedae9095b754448bf62137aa7a7bd90f4b3c0
Praveen Dias <praveendias1180@gmail.com>
2022-05-14 09:06:27 +0530

    Updated the monster

--- oop.py
+++ oop.py
@@ -1,8 +1,11 @@
 class Monster:
+
+    color = "black"
+
     def __init__(self, age, name):
         self.age = age
         self.name = name
+        self.is_innocent = False
 
     def steal(self, warrior):
         warrior.lose_stick()
-        
