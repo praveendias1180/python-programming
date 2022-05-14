270943fa00526e9ab9faef32c1ba9fa9001592df
Praveen Dias <praveendias1180@gmail.com>
2022-05-14 14:03:08 +0530

    Added a warrior

--- oop.py
+++ oop.py
@@ -18,3 +18,24 @@ monster_2 = Monster(15, "MoN")
 print(monster_1.age)
 print(monster_1._is_innocent)
 print(type(monster_2))
+
+class Warrior:
+    def __init__(self, name):
+        self.name = name
+        self._age = 0
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        if age > 0:
+            self._age = age
+        else:
+            print("Age should be greater than zero")
+
+warrior = Warrior('WoR')
+
+warrior.age = 5
+warrior.age = 0
