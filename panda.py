0d8fcd43657134daf121ac9dad65f90ec991cef0
Praveen Dias <praveendias1180@gmail.com>
2022-05-14 21:25:14 +0530

    Panda CSV

--- panda.py
+++ panda.py
@@ -1,12 +1,23 @@
 import pandas as pd
 
 data = {"calories": [420, 380, 390], "duration": [50, 40, 45]}
-print(data)
+# print(data)
 
 # load data into a DataFrame object:
 df = pd.DataFrame(data)
 
-print(df)
+# print(df)
 
 df = pd.read_csv("pandadata.csv")
-print(df.to_string())
+# print(df.to_string())
+
+print(df.head(3))
+
+print('......')
+print(df.dropna())
+
+print('......')
+print(df.fillna(88))
+
+print('......')
+print(df.drop_duplicates())
