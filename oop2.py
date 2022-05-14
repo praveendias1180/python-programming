8cae27c87e5bdc312e10e64159cc1bb29c9cba76
Praveen Dias <praveendias1180@gmail.com>
2022-05-14 14:33:28 +0530

    Super warrior fighting implemented

--- oop2.py
+++ oop2.py
@@ -18,5 +18,17 @@ class NormalWarrior(Warrior):
     def walk(self):
         print("Walking")
 
+class SuperWarrior(Warrior):
+    def fight(self):
+        print("Fighting")
+        return 225
+
+    def walk(self):
+        while self.is_mobile:
+            print("Walk without REST")
+
 n_warrior = NormalWarrior(15, 'WoR')
 print(n_warrior.name)
+
+s_warrior = SuperWarrior(22, 'SoR')
+print(s_warrior.fight())
